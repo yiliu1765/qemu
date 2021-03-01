bd4b3607c5046a5f8c326970429b3b14781cad30
Yi Liu <yi.l.liu@intel.com>
2021-03-01 20:08:53 +0800

    intel_iommu: bind/unbind guest page table to host
    This patch captures the guest PASID table entry modifications and
    propagates the changes to host to setup dual stage DMA translation.
    The guest page table is configured as 1st level page table (GVA->GPA)
    whose translation result would further go through host VT-d 2nd
    level page table(GPA->HPA) under nested translation mode. This is the
    key part of vSVA support, and also a key to support IOVA over 1st-
    level page table for Intel VT-d in virtualization environment.
    
    rfcv10 -> rfcv11:
    *) Add @error_happened in struct VTDPASIDCacheInfo to track the
       bind/unbind failure, and return to guest.
    
    Cc: Kevin Tian <kevin.tian@intel.com>
    Cc: Jacob Pan <jacob.jun.pan@linux.intel.com>
    Cc: Peter Xu <peterx@redhat.com>
    Cc: Yi Sun <yi.y.sun@linux.intel.com>
    Cc: Paolo Bonzini <pbonzini@redhat.com>
    Cc: Richard Henderson <rth@twiddle.net>
    Signed-off-by: Yi Liu <yi.l.liu@intel.com>

--- include/hw/i386/intel_iommu.h
+++ include/hw/i386/intel_iommu.h
@@ -66,6 +66,7 @@ typedef struct VTDPASIDEntry VTDPASIDEntry;
 typedef struct VTDIOMMUFDDevice VTDIOMMUFDDevice;
 typedef struct VTDPASIDCacheEntry VTDPASIDCacheEntry;
 typedef struct VTDPASIDAddressSpace VTDPASIDAddressSpace;
+typedef struct VTDHwpt VTDHwpt;
 
 /* Context-Entry */
 struct VTDContextEntry {
@@ -103,14 +104,23 @@ struct pasid_key {
     uint16_t sid;
 };
 
+struct VTDHwpt {
+    uint32_t hwpt_id;
+    int iommufd;
+    uint32_t parent_id; /* ioas_id or hwpt_id */
+    uint32_t users;
+};
+
 struct VTDPASIDCacheEntry {
     struct VTDPASIDEntry pasid_entry;
+    bool cache_filled;
 };
 
 struct VTDPASIDAddressSpace {
     VTDBus *vtd_bus;
     uint8_t devfn;
     uint32_t pasid;
+    VTDHwpt hwpt;
     IntelIOMMUState *iommu_state;
     VTDContextCacheEntry context_cache_entry;
     QLIST_ENTRY(VTDPASIDAddressSpace) next;
@@ -297,6 +307,7 @@ struct IntelIOMMUState {
 
     /* list of VTDIOMMUFDDevices */
     QLIST_HEAD(, VTDIOMMUFDDevice) vtd_idev_list;
+    VTDHwpt *s2_hwpt;
 
     /* interrupt remapping */
     bool intr_enabled;              /* Whether guest enabled IR */
