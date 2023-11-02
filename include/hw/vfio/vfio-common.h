c25c5d0efd5d9aa89a15279299eb78204218759d
Zhenzhong Duan <zhenzhong.duan@intel.com>
2023-11-02 15:12:56 +0800

    vfio/pci: Make vfio cdev pre-openable by passing a file handle
    This gives management tools like libvirt a chance to open the vfio
    cdev with privilege and pass FD to qemu. This way qemu never needs
    to have privilege to open a VFIO or iommu cdev node.
    
    Together with the earlier support of pre-opening /dev/iommu device,
    now we have full support of passing a vfio device to unprivileged
    qemu by management tool. This mode is no more considered for the
    legacy backend. So let's remove the "TODO" comment.
    
    Add helper functions vfio_device_set_fd() and vfio_device_get_name()
    to set fd and get device name, they will also be used by other vfio
    devices.
    
    There is no easy way to check if a device is mdev with FD passing,
    so fail the x-balloon-allowed check unconditionally in this case.
    
    There is also no easy way to get BDF as name with FD passing, so
    we fake a name by VFIO_FD[fd].
    
    Signed-off-by: Zhenzhong Duan <zhenzhong.duan@intel.com>

--- include/hw/vfio/vfio-common.h
+++ include/hw/vfio/vfio-common.h
@@ -251,4 +251,7 @@ int vfio_devices_query_dirty_bitmap(VFIOContainerBase *bcontainer,
                                     hwaddr size);
 int vfio_get_dirty_bitmap(VFIOContainerBase *bcontainer, uint64_t iova,
                                  uint64_t size, ram_addr_t ram_addr);
+
+int vfio_device_get_name(VFIODevice *vbasedev, Error **errp);
+void vfio_device_set_fd(VFIODevice *vbasedev, const char *str, Error **errp);
 #endif /* HW_VFIO_VFIO_COMMON_H */
