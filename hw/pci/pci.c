73ae59ff3737f3e7bd3ad2a660aa4e69cb20e559
Zhenzhong Duan <zhenzhong.duan@intel.com>
2024-11-27 03:06:07 -0500

    Revert "pci: Unregister BARs before device exit"
    This reverts commit 7cf1b0fd95657e722b2ad290649eee88d1365786.
    
    As TDX Connect architecture requires TDI to be stopped first of
    all during TDI teardown flow (e.g. in case, w/o permission from
    TD to remove MMIO mapping). So ensure this sequence by moving
    exit() before pci_unregister_io_regions().
    
    The original commit fixed an UAF issue that exit functions destroy
    memory regions pci_unregister_io_regions() will reference. This is
    not an issue any more since we use finalize function to destroy
    them.
    
    Signed-off-by: Zhenzhong Duan <zhenzhong.duan@intel.com>

--- hw/pci/pci.c
+++ hw/pci/pci.c
@@ -1432,14 +1432,13 @@ static void pci_qdev_unrealize(DeviceState *dev)
     PCIDevice *pci_dev = PCI_DEVICE(dev);
     PCIDeviceClass *pc = PCI_DEVICE_GET_CLASS(pci_dev);
 
-    pci_unregister_io_regions(pci_dev);
-    pci_del_option_rom(pci_dev);
-    pcie_sriov_unregister_device(pci_dev);
-
     if (pc->exit) {
         pc->exit(pci_dev);
     }
 
+    pci_unregister_io_regions(pci_dev);
+    pci_del_option_rom(pci_dev);
+    pcie_sriov_unregister_device(pci_dev);
     pci_device_deassert_intx(pci_dev);
     do_pci_unregister_device(pci_dev);
 
